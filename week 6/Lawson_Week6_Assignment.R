861c67bc8749a08bab7a085026e714c1cc356940
mattielawson <lawsonm@susqu.edu>
2022-10-10 17:34:50 -0400

    confusion

--- week 6/Lawson_Week6_Assignment.R
+++ week 6/Lawson_Week6_Assignment.R
@@ -5,17 +5,19 @@ df <- read.csv("Toscano_Griffen_Data.csv")
 head(df)
 library(MASS)
 library(MuMIn)
-#library(mgcv)
 
 # First create models with the same (y) and method (GLMM) as the published paper, using the GLMM function from the tutorial. 
   #Create two different models using the same 3 predictor (x) variables from the dataset. (4 points each) 
     # In one model only include additive effects.
-glmm.mod <- glmmPQL(activity.level~prey, family = binomial, random = ~ 1 | ID, data = df)
+glmm.mod1 <- glmmPQL(activity.level~prey, family = binomial, random = ~ 1 | block, data = df)
+summary(glmm.mod1)
+r.squaredGLMM(glmm.mod1)
     # In the other model include one interactive effect.
-glmm.mod <- glmmPQL(activity.level~eaten, family = binomial, random = ~ 1 | ID, data = df)
+glmm.mod2 <- glmmPQL(activity.level~eaten, family = binomial, random = ~ 1 | block, data = df)
+summary(glmm.mod2)
+r.squaredGLMM(glmm.mod2)
     # Use a binomial distribution and block as a random effect in both models to match the paper's analyses. Remember ?family to find distribution names.
 ?family
-
 
 
 # The authors used proportional consumption of prey as the (y) in their model, but did not include this in the dataset.
@@ -23,16 +25,29 @@ glmm.mod <- glmmPQL(activity.level~eaten, family = binomial, random = ~ 1 | ID, 
 df$prop.cons <- df$eaten/df$prey
 
 # (Q1) - The code in line 8 is performing two operations at once. What are they? (2 pts)
-
+    #it is creating a new variable in the data frame, and it is combining the eaten numbers with the
+    #prey numbers to make the proportional consumption of prey so that we can use it as the y
+    #variable instead of just using one or the other.
 
 # (Q2) - Did the interactive effect change which variables predict proportional consumption? How, specifically, did the results change? (5 pts)
-
+glmm.mod3 <- glmmPQL(activity.level~prop.cons, family = binomial, random = ~ 1 | block, data = df)
+summary(glmm.mod3)  
+    #The interactive effect did change the variables that predict proportional consumption.
+    #The results changed as it made the residual smaller, the p-value was higher than when
+    #just comparing to eaten.
 
 # (Q3) - Plot the residuals of both models. Do you think either model is a good fit? Why or why not? (3 pts)
 
 
 # Re-run both models as generalized additive models instead (using gam). Then compare the AIC of both models. (4 points each)
+library(mgcv)
+gam.mod2 <- gam(activity.level~ prey, family = binomial, random = list(ID=~ 1), data = df)
+summary(gam.mod2)
+#AIC:
 
+gam.mod1 <- gam(activity.level~ prop.cons, family = binomial, random = list(ID=~ 1), data = df)
+summary(gam.mod1)
+#AIC:
 
 # (Q4) - Which model is a better fit? (2 pt)
 
