245dfde929ff30115670f6209bf5518ffb740e6d
Matt Wilson <wilsonmatt@susqu.edu>
2022-10-14 13:40:23 -0400

    Wk6 graded

--- week 6/Lawson_Week6_Assignment.R
+++ week 6/Lawson_Week6_Assignment.R
@@ -18,7 +18,7 @@ summary(glmm.mod2)
 r.squaredGLMM(glmm.mod2)
     # Use a binomial distribution and block as a random effect in both models to match the paper's analyses. Remember ?family to find distribution names.
 ?family
-
+#need three predictor variables in each model! You setup everything right, but needed activity~prey+eaten+cue and activity~prey*eaten+cue (or something like that)
 
 # The authors used proportional consumption of prey as the (y) in their model, but did not include this in the dataset.
   # So we are going to create it - run the following line, assuming df= your data frame (feel free to change that):
@@ -35,6 +35,7 @@ summary(glmm.mod3)
     #The interactive effect did change the variables that predict proportional consumption.
     #The results changed as it made the residual smaller, the p-value was higher than when
     #just comparing to eaten.
+  #Where's the model with the interactive effect?
 
 # (Q3) - Plot the residuals of both models. Do you think either model is a good fit? Why or why not? (3 pts)
 plot(glmm.mod1)
@@ -42,6 +43,7 @@ plot(glmm.mod2)
     #I think that both of the residuals are both very random, but glmm.mod2 seems to be a better
     #fit because the glmm.mod1 seems to have data points forming in vertical lines in the plot
     #but glmm.mod2 is more random
+#I would say they both have some patterns going on (horizontal patterns in the second model) but in general agree.
 
 # Re-run both models as generalized additive models instead (using gam). Then compare the AIC of both models. (4 points each)
 library(mgcv)
@@ -58,3 +60,5 @@ AIC(gam.mod1)
 
 # (Q5) - Based on the residuals of your generalized additive models, how confident are you in these results? (2 pts)
     #Honestly, not very personally confident 
+#Where are the residual plots for the gams?
+
