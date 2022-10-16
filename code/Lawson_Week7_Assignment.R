e38e64f799c3d134f12f82bbdb031902839a7a25
mattielawson <lawsonm@susqu.edu>
2022-10-16 16:40:30 -0400

    777

---
+++ code/Lawson_Week7_Assignment.R
@@ -0,0 +1,61 @@
+# Load the "anytime" and "ggplot2" packages to complete this week's assignment.
+library("anytime")
+library("ggplot2")
+
+# Read the "Plankton_move_average" CSV in from GitHub. 
+# These are data from the Great Lakes Environmental Research Laboratory plankton sampling.
+setwd("C:/GitHub/lawsonle/week 7")
+data <- read.csv("Plankton_move_average.csv")
+
+#Used the following lines to format the date and remove NAs from the dataset:
+data$Date <- as.Date(data$Date, origin = "0001-01-01") # Setting values to "day zero".
+data <- na.omit(data)
+
+#Plot these population data over time with the following code:
+ggplot(data)  +
+  xlab("Numeric Date") + ylab("Density Individuals")+
+  geom_line(data=data, aes(Date, D.mendotae), color="black", alpha = 0.7, size=1)+
+  geom_line(data=data, aes(Date, LimncalanusF+LimncalanusM), color="orange",  alpha = 0.7, size=1)+ # adding males and females together, hint: this is actually spelled Limnocalanus
+  geom_line(data=data, aes(Date, Bythotrephes), color="sky blue",  alpha = 0.7, size=1)+
+  geom_line(data=data, aes(Date, Bythotrephes), color="sky blue",  alpha = 0.7, size=1)+
+  theme_bw() 
+
+# Export this plot to have on hand for reference in the next section of the assignment (and upload with your script).
+
+# (1) - Which species is most likely to be r-selected prey and which its primary predator? (2 pts)
+  #The species most likely to be r-selected prey is the one represented by the black line.
+  #It's primary predator would be the one represented by the yellow line
+# What is one relationship the third species MIGHT have to the first two? (2 pts)
+  #One relationship the third species might have on the other two is that it could be something
+  #that is effected by environmental changes the relationship between the other two causes
+
+#Now copy/paste in the Lotka-Volterra function, plotting script, and load the "deSolve" package from the tutorial:
+library("deSolve")
+LotVmod <- function (Time, State, Pars) {
+  with(as.list(c(State, Pars)), {
+    dx = x*(alpha - beta*y)
+    dy = -y*(gamma - delta*x)
+    return(list(c(dx, dy)))
+  })
+}
+Pars <- c(alpha = 2, beta = 0.5, gamma = .2, delta = .6) 
+State <- c(x = 10, y = 10)
+Time <- seq(0, 100, by = 1)
+out <- as.data.frame(ode(func = LotVmod, y = State, parms = Pars, times = Time)) 
+matplot(out[,-1], type = "l", xlab = "time", ylab = "population")
+
+# (2) - What do alpha, beta, gamma, and delta represent in this function? (4 pts)
+  #alpha represents the rate of prey population growth
+  #beta represents the rate of predation
+  #delta represents rate of prey consumption/ rate of prey die-off 
+
+# (3) - By only changing values for alpha, beta, gamma, and/or delta
+# change the default parameters of the L-V model to best approximate the relationship between Limncalanus and D.mendotae, assuming both plots are on the same time scale.
+# What are the changes you've made to alpha, beta, gamma, and delta from the default values; and what do they say in a relative sense about the plankton data? (4 pts)
+# Are there other paramenter changes that could have created the same end result? (2 pts)
+# Export your final L-V plot with a legend that includes the appropriate genus and/or species name as if the model results were the real plankton data, 
+# and upload with your script. (hint - remember which one is the predator and which is the prey)
+
+
+
+
