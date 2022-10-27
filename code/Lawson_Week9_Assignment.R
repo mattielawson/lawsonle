c47aa9844eebed37a55bcc3df6718bb26ff0b139
mattielawson <lawsonm@susqu.edu>
2022-10-27 09:52:08 -0400

    ?

---
+++ code/Lawson_Week9_Assignment.R
@@ -0,0 +1,35 @@
+# For this week it's time to start exploring your own ideas and questions in R.
+  # There are at least five options in the dataset to create the following comparisons.
+
+# (Q1 - 12 pts) Use the dataset from the tutorial to complete one redundancy analysis (RDA) with variance partitioning on a different community (NOT the nematodes).
+  # Explain the ecological importance of your significant predictor variables, or the importance if none are significant for your community.
+library(readxl)
+setwd("C:/GitHub/lawsonle/week 9")
+
+abiotic.tibble <- read_excel("Penaetal_2016_data.xlsx", sheet = "Abiotic factors")
+abiotic <- as.data.frame(abiotic.tibble)
+head(abiotic)
+
+invert.tibble <- read_excel("Penaetal_2016_data.xlsx", sheet = "Invertebrate_community")
+invert <- as.data.frame(invert.tibble)
+head(invert)
+
+abiotic.names <- paste(abiotic$Parcel, abiotic$Land_Use)
+abiotic$names <- abiotic.names
+head(abiotic)
+
+invert.names <- paste(invert$Parcel, invert$Landuse)
+invert$names <- invert.names
+head(invert)
+
+abiotic.means <- aggregate(x = abiotic, by = list(abiotic$names), FUN = "mean")
+head(abiotic.means)
+invert.means <- aggregate(x = invert, by = list(invert$names), FUN = "mean")
+head(invert.means)
+
+# (Q2 - 12 pts) Then use the dataset from the tutorial to create a linear model related to your RDA. Try multiple predictors to find the best fit model.
+  # Explain the ecological importance of the significant predictors, or lack of significant predictors.
+
+# (Q3 - 6 pts) Provide a 3-4 sentence synthesis of how these results relate to one another and the value of considering both together for interpreting biotic-abiotic interactions.
+
+
