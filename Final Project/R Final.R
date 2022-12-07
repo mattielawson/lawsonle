379bf7638971dcbec8805cd2baa6882da2c65507
mattielawson <lawsonm@susqu.edu>
2022-12-07 17:09:21 -0500

    more progress?

--- Final Project/R Final.R
+++ Final Project/R Final.R
@@ -7,7 +7,7 @@ bear1 <- read.csv("YWW Black Bear Data.csv")
 beardf1 <- as.data.frame(bear1)
 head(bear1)
 
-?aggregate
+#Sum of how many total black bear were seen by date - get rid of unneeded columns
 bear_ag1 <- aggregate(bear1$how_many, by=list(bear1$date), FUN = "sum")
 
 
@@ -17,5 +17,22 @@ bear2 <- read.csv("Remote_camera_data.csv")
 beardf2 <- as.data.frame(bear2)
 head(bear2)
 
+###Sum of how many total black bear were seen by date - get rid of unneeded columns 
 bear_ag2 <- aggregate(bear2$BlackBear, by=list(bear2$Date), FUN ="sum")
+
+###Change date format in data set to match with other
+bear_ag2$Date <- format(as.POSIXct(bear_ag2$Group.1,format='%d-%m-%Y'),format='%m/%d/%Y')
+
+bear_ag2$Date <- format(as.Date(bear_ag2$Group.1,format='%d-%m-%Y'),format='%m/%d/%Y')
+?as.Date
+as.Date(as.character(bear_ag2$Group.1),format='%d/%m/%Y')
+
+
+data.frame(do.call("rbind", strsplit(as.character(bear_ag2$Group.1), "-", fixed = TRUE)))
+
+
+
+
+
+
 
