021663c513fbc74ca0c680ad1085cda539e0e8be
mattielawson <lawsonm@susqu.edu>
2022-12-08 09:50:12 -0500

    progress

--- Final Project/R Final.R
+++ Final Project/R Final.R
@@ -1,3 +1,4 @@
+##DATA GROOMING
 #Set working directory
 setwd("C:/GitHub/lawsonle/Final Project")
 
@@ -17,19 +18,33 @@ bear2 <- read.csv("Remote_camera_data.csv")
 beardf2 <- as.data.frame(bear2)
 head(bear2)
 
-###Sum of how many total black bear were seen by date - get rid of unneeded columns 
+#Sum of how many total black bear were seen by date - get rid of unneeded columns 
 bear_ag2 <- aggregate(bear2$BlackBear, by=list(bear2$Date), FUN ="sum")
 
-###Change date format in data set to match with other
-bear_ag2$Date <- format(as.POSIXct(bear_ag2$Group.1,format='%d-%m-%Y'),format='%m/%d/%Y')
-
-bear_ag2$Date <- format(as.Date(bear_ag2$Group.1,format='%d-%m-%Y'),format='%m/%d/%Y')
-?as.Date
-as.Date(as.character(bear_ag2$Group.1),format='%d/%m/%Y')
-
-
+#split apart dates so day, month, and year are all separate for first data set
 df1<-data.frame(do.call("rbind", strsplit(as.character(bear_ag2$Group.1), "-", fixed = TRUE)))
-cbind(bear_ag2,df1)
+#combine date parts to original data frame 
+df3 <- cbind(bear_ag2,df1)
+#split apart dates so day, month, and year are all separate for secind data set
+df2<-data.frame(do.call("rbind", strsplit(as.character(bear_ag1$Group.1), "-", fixed = TRUE)))
+#combine date parts to original data frame
+df4<- cbind(bear_ag1, df2)
+
+#Subset for only month and number of bears seen for each data set
+df5 <- subset(df4, select=-c(Group.1,X1,X3))
+df6 <- subset(df3, select=-c(Group.1,X1,X3))
+
+#rbind both data sets together to put all data in one place
+final<-rbind(df5, df6)
+
+#change names in final data frame to what they represent (bears & month)
+colnames(final)[1]="bears"
+colnames(final)[2]="month"
+
+#DATA ANALYSIS
+
+#boxplot
+boxplot(bears~month, data=final, main="Bears Seen in Each Month",ylim=c(0,100))
 
 
 
